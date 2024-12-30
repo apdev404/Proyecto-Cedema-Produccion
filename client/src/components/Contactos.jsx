ad8bc4ec6f24440085ccbfd261e5833e31155df5
apdev404 <agustin.e.pomponio@gmail.com>
2024-12-29 23:48:18 -0300

    Solucion a responsividad de redes sociales en home

--- client/src/components/Contactos.jsx
+++ client/src/components/Contactos.jsx
@@ -3,8 +3,8 @@ import '../css/logo.css';
 
 function Redes() {
     return (
-        <div className="container-fluid">
-            <ul className='list-social'>
+        <div className="container-fluid p-0">
+            <ul className='list-social row'>
                 <li className='social-elements col'><a href='https://www.facebook.com/cedema.arg/'><i className="fa-brands fa-facebook-f social"></i></a></li>
                 <li className='social-elements col'><a href='https://chat.whatsapp.com/IDauAcD6Pmv5pzYhmyGvRP'><i className="fa-brands fa-whatsapp social"/></a></li>
                 <li className="social-elements col"><a href='https://www.youtube.com/@cedemaarg1850'><i className="fa-brands fa-youtube social"/></a></li>
