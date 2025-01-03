04d147c1f28582a55ec24fe1b5f4698da1917a99
Dana <dcossettinireyes@estudiantes.unsam.edu.ar>
2025-01-02 22:03:27 -0300

    cards personal - AboutPage

---
+++ client/src/components/CardsPersonas.jsx
@@ -0,0 +1,58 @@
+import CardPersonal from './CardPersonal';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import '../css/card.css';
+
+function CardsPersonas() {
+    return (
+        <div className="cards-personas-container">
+            <Row className="justify-content-center g-4">
+                <Col xs={12} sm={6} md={4} lg={3}>
+                    <CardPersonal
+                        image='/img-persona1.jpg'
+                        name="Iñaki Agüero Clarembaux"
+                        role="Coordinador General"
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={3}>
+                    <CardPersonal
+                        image='/img-persona2.jpg'
+                        name="Agustina Ortega"
+                        role="Coordinadora de capital Humano y comunicación"
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={3}>
+                    <CardPersonal
+                        image='/img-persona3.jpg'
+                        name="Yesenia Motola"
+                        role="Secretaria de Proyectos"
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={3}>
+                    <CardPersonal
+                        image='/img-persona4.jpg'
+                        name="Anibal Mella"
+                        role="Área de articulación académica internacional"
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={3}>
+                    <CardPersonal
+                        image='/img-persona5.jpg'
+                        name="Valeria Delgado"
+                        role="Área de antártida y atlántico sur"
+                    />
+                </Col>
+                <Col xs={12} sm={6} md={4} lg={3}>
+                    <CardPersonal
+                        image='/img-persona6.jpg'
+                        name="Iñaki Santos"
+                        role="Secretario de relaciones institucionales"
+                    />
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default CardsPersonas;
--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -4,13 +4,14 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
 import '../css/aboutPage.css';
+import CardsPersonas from '../components/CardsPersonas';
 
 export const AboutPage = () => {
   return (
     <section className='section container-fluid aboutPage'>
       <Container className='cedema-head'>
         <Row className='cedema-description'>
-          <Col xs={12} md={6}>
+          <Col xs={{ order: 1 }} md={{ order: 1 }} >
             <h3 className='section-title row encode-sans-condensed-regular' data-aos="flip-up">¿Quiénes somos?</h3>
             <p className='encode-sans-condensed-light' data-aos="fade-up">
               Centro de Debate Multidisciplinario Argentino (CEDEMA) es una comunidad comprometida con el análisis y el debate de los asuntos que suceden al interior de nuestro país como en cuestiones y hechos que tienen lugar en todo el mundo, conformado, por estudiantes, graduados, e intelectuales, de distintas disciplinas y universidades, con diferentes puntos de vistas.
@@ -20,16 +21,16 @@ export const AboutPage = () => {
               Tiene como misión crear una comunidad abierta e inclusiva que se extrapole a un debate en nuestra plataforma, moderna e integradora, centrada en la dialéctica y discusión sobre diversas temáticas debido a la pluralidad de posturas mediante el análisis crítico y argumentativo.
             </p>
           </Col>
-          <Col xs={12} md={6} >
+          <Col xs={{ order: 2 }} md={{ order: 2 }}>
             <Image src="/imgMain-AboutPage.jpg" fluid className='img-1' data-aos="fade-left"/>
           </Col>
         </Row>
-        <Row>
-          <Col xs={12} md={6}>
-            <Image src="/quienes_somos_2.jpg" fluid className='img-2' data-aos="fade-right"/>
+        <Row className='cedema-description'>
+          <Col xs={{ order: 2 }} md={{ order: 1 }}>
+            <Image src="https://i.pinimg.com/736x/19/e8/c2/19e8c2772bbd766bb39127c69ac03da1.jpg" fluid className='img-2' data-aos="fade-right"/>
           </Col>
-          <Col xs={12} md={6}>
-            <h2 className='encode-sans-condensed-regular' data-aos="flip-up">Objetivos</h2>
+          <Col xs={{ order: 1 }} md={{ order: 2 }}>
+            <h1 className='encode-sans-condensed-regular text-title' data-aos="flip-up">Objetivos</h1>
             <ul className='encode-sans-condensed-light' data-aos="fade-up">
               <li>Construir lazos que nos acerquen para enriquecernos académica, social y culturalmente. </li>
               <li>Fomentar el debate y el análisis multidisciplinario en un marco nacional e internacional. </li>
@@ -38,6 +39,23 @@ export const AboutPage = () => {
             </ul>
           </Col>
         </Row>
+        <Row className='cedema-description'>
+          
+          <Col xs={12} md={6}>
+            <h1 className='encode-sans-condensed-regular text-title' data-aos="flip-up" >Titulo</h1>
+            <p className='encode-sans-condensed-light' data-aos="fade-up">
+              Desccripcion: lorLorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ratione debitis maiores natus inventore nisi iusto atque officiis, ad non deserunt dolore aut quia laboriosam obcaecati! Velit quibusdam quam pariatur.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci incidunt, repellat impedit corrupti mollitia ipsam similique tempore? Mollitia repudiandae doloribus, delectus nostrum blanditiis saepe. Ab consectetur omnis voluptatem quisquam soluta!lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus impedit porro, assumenda quaerat ea quo blanditiis, quas debitis suscipit magnam dolor incidunt? Modi libero sapiente hic temporibus molestias iste ipsa?
+              Desccripcion: lorLorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ratione debitis maiores natus inventore nisi iusto atque officiis, ad non deserunt dolore aut quia laboriosam obcaecati! Velit quibusdam quam pariatur.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci incidunt, repellat impedit corrupti mollitia ipsam similique tempore? 
+            </p>
+          </Col>
+          <Col xs={12} md={6}>
+            <Image src="/imgGrupo.jpg" fluid className='img-3' data-aos="fade-right"/>
+          </Col>
+        </Row>
+        <hr data-aos="fade-up"/>
+        <h1 className='encode-sans-condensed-regular text-title' data-aos="flip-up" >Equipo</h1>
+        <CardsPersonas/>
+        
         {/*
           <Row className="justify-content-md-center">
           <h1 className='encode-sans-condensed-regular' data-aos="flip-up">Nos conforman...</h1>
