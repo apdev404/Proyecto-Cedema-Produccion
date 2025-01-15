b6fe5981fd4d6d60f2799e2a555b6f2a0b6403ae
Catalina Correa <catalinayazmincorrea@gmail.com>
2025-01-15 15:20:06 -0300

    import de aos en main y minimo cambio en animacion de novedades de home

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css"; // Importo bootstrap (No hace falta importarlo en ningun otro file)
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom"; // Importo React Router
 
