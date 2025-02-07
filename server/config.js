24d45373691753538bc552a8db1b08513f5a3f15
apdev404 <agustin.e.pomponio@gmail.com>
2025-02-07 18:38:52 -0300

    ...

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export const PORT = proccess.env.PORT || 3000;
+export const DB_HOST = proccess.env.DB_HOST || "localhost";
+export const DB_USER = proccess.env.DB_USER || "root";
+export const DB_PASSWORD = proccess.env.DB_PASSWORD || "apdev404";
+export const DB_NAME = proccess.env.DB_NAME || "web_cedema";
+export const DB_PORT = proccess.env.DB_PORT || 3306;
--- server/database/db.js
+++ server/database/db.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from '../config';
+
 const database = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT // Railway a veces usa puertos distintos
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT // Railway a veces usa puertos distintos
 });
 
 database.connect((error) => {
